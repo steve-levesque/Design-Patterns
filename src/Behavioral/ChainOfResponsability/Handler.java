c2e83cd4b840159472faae2ad0b6ca7ca64bbf2f
Steve Lévesque <steve-levesque@fractalevision.ca>
2019-08-08 09:22:42 -0400

    Design Pattern - Chain of Responsability
    - Separation of main.
    - Changed name of file.

--- src/Behavioral/ChainOfResponsability/Handler.java
+++ src/Behavioral/ChainOfResponsability/Handler.java
@@ -1,6 +1,6 @@
 package Behavioral.ChainOfResponsability;
-
-class Handler {
+ 
+public class Handler {
 	int unit;
 	Handler nextHandler;
 	
@@ -20,20 +20,5 @@ class Handler {
 		System.out.println(withdrawAmmount + " bills of " +unit+ " dollar(s).");
 		if (this.nextHandler != null)
 			this.nextHandler.dispense(withdraw - withdrawAmmount * unit);
-	}
-}
-
-public class ChainOfResponsability {
-	static int[] types = {100, 20, 10, 5, 1};
-	static int withdraw = 5432;
-	
-	public static void main(String[] args) {
-		Handler bank = new Handler(types[0]);
-		bank.add(new Handler(types[1]));
-		bank.add(new Handler(types[2]));
-		bank.add(new Handler(types[3]));
-		bank.add(new Handler(types[4]));
-		
-		bank.dispense(withdraw);
 	}
 }
