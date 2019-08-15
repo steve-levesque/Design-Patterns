32f75d1283965ebbc79f02b02139e5f319579279
Steve Lévesque <steve-levesque@fractalevision.ca>
2019-08-14 21:11:29 -0400

    DP Strategy - Comments
    Added comments for clarity.

--- src/Behavioral/Strategy/Main.java
+++ src/Behavioral/Strategy/Main.java
@@ -6,11 +6,23 @@
  */
 
 package Behavioral.Strategy;
+// The strategy suggests that a method or a class doing an action with multiple
+// implementations different from one another but resulting in the same
+// Answer should be separated into different parts (strategies).
+
+// Pros : 
+// - Adds abstraction between functionalities.
+// - Reduces possible coupling with the current code.
+// Cons :
+// - The selection of a strategy must now be implemented.
 
 class Main {
 	public static void main(String[] args) {
+		// Main class, waiting for the user to select a strategy.
 		Operation work = new Operation(new JuniorProgrammer(), 3);
 		work.executeOperation();
+		
+		// Trying other strategies.
 		work.setEmployee(new Programmer());
 		work.executeOperation();
 		work.setEmployee(new SeniorProgrammer());
