c9a862ea08deef8644370930cffed379e4fd0e8c
Steve Lévesque <steve-levesque@fractalevision.ca>
2019-08-18 09:08:16 -0400

    DP Command - Comments and Rework
    - Added comments for clarity.
    - Added a command class for better clarity.

--- src/Behavioral/Command/Main.java
+++ src/Behavioral/Command/Main.java
@@ -6,26 +6,18 @@
  */
 
 package Behavioral.Command;
-
-import java.util.ArrayList;
+// The command suggests that the business logic of the requests should not be known and to prevent
+// this from happening, a command class should be initialized in the middle. This class would contain
+// all the request objects and a method to trigger all the request's commands.
 
 class Main {
-	public static ArrayList<Calendar> calendar = new ArrayList<Calendar>();
-	
-	public static void addAppointments() {
-		calendar.add(new Evening());
-		calendar.add(new Day());
-		calendar.add(new Night());
-		System.out.println("Appointments added to the calendar.");
-	}
-	
-	public static void showAppointments() {
-		System.out.println("Showing appointments in the calendar...");
-		calendar.forEach((n) -> n.Appointment()); 
-	}
-
 	public static void main(String[] args) {
-		addAppointments();
-		showAppointments();
+		Command command = new Command();
+		
+		command.addAppointments(new Evening());
+		command.addAppointments(new Day());
+		command.addAppointments(new Night());
+		
+		command.showAppointments();
 	}
 }
