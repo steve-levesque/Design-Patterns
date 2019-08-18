fd0f341e5518adf34f7368265a0118dbabeda0e5
Steve Lévesque <steve-levesque@fractalevision.ca>
2019-08-18 11:52:46 -0400

    DP Prototype - Comments
    - Added comments for clarity.

---
+++ src/Creational/Prototype/Prototype.java
@@ -0,0 +1,27 @@
+package Creational.Prototype;
+
+import java.util.ArrayList;
+
+public class Prototype {
+	private ArrayList<Employee> employees;
+	
+	Prototype() {
+		employees = new ArrayList<Employee>();
+	}
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	public void getEmployeeNames() {
+		employees.forEach((emp) -> System.out.println(emp.getName()));
+	}
+	
+	public void cloneAll(int multiplicator) {
+		employees.forEach((emp) -> {
+				for (int i = 0; i < multiplicator; i++)
+					System.out.println(emp.clone().getName());
+			}
+		);
+	}
+}
