a7f5305b75f1b4793ffe73c7201331081bb15403
Steve Lévesque <steve-levesque@fractalevision.ca>
2019-08-21 14:23:19 -0400

    DP Builder - Comments
    - Added comments for clarity.

--- src/Creational/Builder/Computer.java
+++ src/Creational/Builder/Computer.java
@@ -6,6 +6,8 @@
  */
 
 package Creational.Builder;
+// The object doesn't need to implement a huge constructor that would
+// force the attribution of variables even if not necessary.
 
 class Computer {
 	// Simplified for example purpose.
--- src/Creational/Builder/Main.java
+++ src/Creational/Builder/Main.java
@@ -6,6 +6,17 @@
  */
 
 package Creational.Builder;
+// The builder suggests to move all the code that builds the object into a separate class,
+// a builder, instead of doing it in its constructor. This avoids the initialization of
+// objects with huge constructors which might actually use some of the parameters 1 time out of
+// 100.
+
+// Pros :
+// - Reduces object construction needed parameters.
+// - Removes the need of sub classes for each object variant.
+// Cons :
+// - Huge number of steps and different types of builders can result in the need of the director class.
+// - The director is not meant to return objects.
 
 class Main {
 	public static void main(String[] args) {
@@ -13,7 +24,7 @@ class Main {
 		
 		ComputerBuilder builder = new ComputerBuilder();
 		
-		// THe director only executes plans of construction.
+		// The director only executes plans of construction.
 		director.makeDesktopComputer(builder);
 		director.makePortableGamingComputer(builder);
 		
